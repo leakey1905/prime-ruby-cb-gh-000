ed41f2081abf3f0cb8a68dce417e948df77190c9
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 12:01:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1 || number % 2 == 0
+    return false
+  elsif number == 2 || number == 3
+    return true
+  else
+    i = 2
+    while i * i <= number
+      if number % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
+end

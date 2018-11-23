a6cc8ea7f75f67cecc78b9fe95c845d78384f86f
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 12:01:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,16 @@
 # Add  code here!
-def prime?()
-
+def prime?(number)
+  if number <= 1 || number % 2 == 0
+    return false
+  elsif number == 2 || number == 3
+    return true
+  else
+    i = 2
+    while i * i <= number
+      if number % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
 end
